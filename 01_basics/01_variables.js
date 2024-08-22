3ede83dcd0253d5e916ece593ef54b51c855dd76
Suryaraj Baranwal <91749712+b-suryaraj@users.noreply.github.com>
2024-08-22 06:22:12 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14445643
+let accountEmail = "surya@mail.com"
+var accountPassword = "12345"
+accountCity = "Asansol"
+let accountState; //initialized with "undefined" if not manually stated
+
+// accountId = 2  // not allowed
+
+accountEmail="abc@mail.com"
+accountPassword="qwerty"
+accountCity="Bokaro"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //it won't work as we are using node.js , not browser
+
+//Code Readability should be highe
+console.log(3+3)
+console.log("Surya")
+
+let num=123;
+
+//PRIMITIVE DATATYPES
+//number => 2 to power 54
+//bigint => larger numbers
+//string => enclosed within "" or ''("" is preffered)
+//boolean => true/false
+//null => standalone value("no value")
+//undefined => value uninitialized
+//symbol => unique value
+
+//NON PRIMITIVE DATATYPES
+//Object
+
+
+//typeOf is used to get the datatype of the mentioned alongside variable 
+console.log(typeof num);  //number
+console.log(typeof null);  //Object
+console.log(typeof undefined)  //undefined
