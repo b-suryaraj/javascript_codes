c5a200eaab84438cba684783962c7b4e6afde4b4
Suryaraj Baranwal <91749712+b-suryaraj@users.noreply.github.com>
2024-09-22 01:41:14 +0000

    Arrays added

--- 02_basics2/01_arrays.js
+++ 02_basics2/01_arrays.js
@@ -12,10 +12,43 @@
 const arr = [0,1,2,3,4]
 const arr2  = new Array(1,2,3,4)
 
-console.log(arr[0]) //element at 0 index(1st pos)
-console.log(arr2[0]) //element at 0 index(1st pos)
+// console.log(arr[0]) //element at 0 index(1st pos)
+// console.log(arr2[0]) //element at 0 index(1st pos)
 
 //arr methods
-arr.push(6);
-arr.push(7);
-arr.pop();
+// arr.push(6);
+// arr.push(7);
+// arr.pop();
+
+//adding elements in front
+// arr.unshift(9)
+//removing elements from front
+// arr.shift()
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+
+// const newArr = arr.join()
+
+// console.log(typeof arr);
+// console.log(arr);
+// console.log(typeof newArr);
+// console.log(newArr);
+
+//slice , splice method
+
+console.log("A ",arr);          //A  [ 0, 1, 2, 3, 4 ]
+
+const myn1=arr.slice(1,3)       // [ 1, 2 ]
+console.log(myn1);              // B  [ 0, 1, 2, 3, 4 ]
+console.log("B ",arr);
+
+const myn2=arr.splice(1,3)      
+console.log(myn2);              //[ 1, 2, 3 ]
+console.log("C ",arr);          // C [0, 4 ]
+
+
+/* slice is different from splice in two ways:
+    -> slice range is exclusive in nature whereas splice is inclusive
+    -> slice just extracts the given range of elements whereas splice also remove the range of elements from the arr
+*/
